440fe6f81abd5db712376dcc1a59bceb52ff8cc0
a71143055 <a71143055@gmail.com>
2025-06-02 15:09:22 +0900

    ActionBarAndFragment

---
+++ app/src/main/java/kr/ac/kopo/actionbarandfragment/MyTabFragment.java
@@ -0,0 +1,35 @@
+package kr.ac.kopo.actionbarandfragment;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class MyTabFragment extends Fragment {
+    String tabName;
+    public void oncreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Bundle data = getArguments();
+        tabName = data.getString("tabname");
+    }
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT
+        );
+        LinearLayout baseLayout = new LinearLayout(super.getActivity());
+        baseLayout.setOrientation(LinearLayout.VERTICAL);
+        baseLayout.setLayoutParams(params);
+
+        if (tabName.equals("Spring")) baseLayout.setBackgroundColor(Color.RED);
+        if (tabName.equals("Summer")) baseLayout.setBackgroundColor(Color.GREEN);
+        if (tabName.equals("Autumn")) baseLayout.setBackgroundColor(Color.BLUE);
+
+        return baseLayout;
+    }
+}
